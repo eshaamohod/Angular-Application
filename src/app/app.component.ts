333a88e05c3cbc38cad191e1154486829fa00e49
Eshaa <mohodeshaa@gmail.com>
2022-04-25 00:32:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular2c';
+  title = 'angular';
+  getValues(val:any)
+  {
+    console.log(val);
+  }
 }
